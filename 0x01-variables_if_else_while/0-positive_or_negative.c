da7c3d8c9ae961fd718712c0b4a330c4de581cd7
kwasicode <collinshicent@gmail.com>
2023-09-08 08:27:38 -0700

    determine values of n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-#inlcude <stdio.h>
+#include <stdio.h>
+
 /**
- *  main - Determines if a number is positive, negative or zero
- *  Return: Always 0 (Success)
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,15 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if(n==0)
+	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
-	else 
-		printf("%d is less than zero\n",n);
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
