50afca40c96b42886bf8e48d816d91ba249d39bd
kwasicode <collinshicent@gmail.com>
2023-09-12 12:59:28 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,24 +12,24 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
+
 			if (j == 0)
 			{
-				_putchar('0' + result);
+				_putchar('0');
 			}
 			else
 			{
-				_putchar(' ');
-				if (result < 10)
-					_putchar(' ');
-				else
-					_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			if (j < 9)
-			{
 				_putchar(',');
 				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
 			}
+
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
 		}
 		_putchar('\n');
 	}
