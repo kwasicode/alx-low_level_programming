ca1bbb00af3eb5f21b772e0bcff135bcc761d0f4
kwasicode <collinshicent@gmail.com>
2023-09-12 13:05:25 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,12 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table
+ * main - Entry point of the program
+ *
+ * Return: Always 0
  */
-void times_table(void)
+int main(void)
 {
-	int i, j, result;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
-
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (result < 10)
-				{
-					_putchar(' ');
-				}
-			}
-
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-		}
-		_putchar('\n');
-	}
+    times_table();
+    return (0);
 }
