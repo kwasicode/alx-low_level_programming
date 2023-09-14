25845fa3b37956d258432ea6b906d0e4d59c44e8
kwasicode <collinshicent@gmail.com>
2023-09-14 12:00:35 -0700

    nested loop for matrix

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times.
+ */
+void more_numbers(void)
+{
+	int num, count;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
