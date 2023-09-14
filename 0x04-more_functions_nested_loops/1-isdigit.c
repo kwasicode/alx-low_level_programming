06a1f4492da8c241737dda7a90411d69e8bf8950
kwasicode <collinshicent@gmail.com>
2023-09-14 09:48:14 -0700

    checks for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - Checks for digits
+ *@c: inpute value
+ *Return: returns 1 if digit and 0 is not digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
