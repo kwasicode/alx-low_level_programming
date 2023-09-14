675fb15513e3ab00cb6cf7a0e5de190e044e485c
kwasicode <collinshicent@gmail.com>
2023-09-14 10:09:00 -0700

    adjusted

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,14 @@
 /**
  *_isdigit - Checks for digits
  *@c: inpute value
- *Return: returns 1 if digit and 0 is not digit
+ *Return: returns 1 if digit and 0 is n
  */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9)
+	if (c >= '0' && c <= '9')
 		return (1);
+
 	else
 		return (0);
 }
