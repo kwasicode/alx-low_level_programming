65b2671ff50c6d895e40ac9bbeb6d059017b323a
kwasicode <collinshicent@gmail.com>
2023-09-14 10:23:21 -0700

    multipies 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies 2 integers.
+ * @a: Integer 1 to be multiplied.
+ * @b: Integer 2 to be multiplied.
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
