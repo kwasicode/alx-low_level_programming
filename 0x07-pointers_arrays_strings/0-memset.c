a3f5e8244ad6f496ca8e488ce10a41ee8178b204
kwasicode <collinshicent@gmail.com>
2023-09-25 07:46:50 -0700

    fills a memory block

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory with a specific value
+ * _memset - fills a memory block with a specific value
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
